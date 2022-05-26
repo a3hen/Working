73cc2bba3f078ee1c1f0eaf6e25ad2010f41a4be
a3hen <8675391+a3hen@user.noreply.gitee.com>
2022-05-26 14:25:19 +0800

    第四次提交

--- Connection.py
+++ Connection.py
@@ -4,30 +4,31 @@ import paramiko
 
 class Ssh :
 
-    def __init__(self,ip,port,username,passwd,command):
+    def __init__(self,ip,port,username,passwd):
         self.ip = ip
         self.port = port
         self.username = username
         self.passwd = passwd
-        self.command = command
+        self.ssh = paramiko.SSHClient()
 
-    def method_processing_data(self):
-        ssh = paramiko.SSHClient()
+    def start_connection(self):
         know_host = paramiko.AutoAddPolicy()
-        ssh.set_missing_host_key_policy(know_host)
-
-        ssh.connect(
+        self.ssh.set_missing_host_key_policy(know_host)
+        self.ssh.connect(
             hostname = self.ip,
             port = self.port,
             username = self.username,
             password = self.passwd
                     )
 
-        stdin, stdout, stderr = ssh.exec_command(self.command)
+    def stay_connection(self,command):
+        stdin, stdout, stderr = self.ssh.exec_command(command)
         output_info = stdout.read().decode('utf-8')
-        ssh.close()
-
         return output_info
+
+    def close_connection(self):
+        self.ssh.close()
+
 
 def filter_data(keyword,data):
     processing = re.findall(r'(\w{8}-\w{4}-\w{4}-\w{4}-\w{12})  [\w\s]{1,8}  '+keyword,data)
--- implement_reads.py
+++ implement_reads.py
@@ -7,16 +7,19 @@ import Connection
 def filter_Operation(yaml_name,keyword) :
         with open(yaml_name, 'r', encoding='utf-8') as f:
             result = yaml.load(f, yaml.FullLoader)
+
         for i in range(len(result['Host'])):
+            host = result['Host'][i]["IP"]
+            test_SSH = Connection.Ssh(result['Host'][i]["IP"], result['Host'][i]["Port"], result['Host'][i]["User"],result['Host'][i]["Password"])
+            test_SSH.start_connection()
             for j in range(len(result['Command'])):
-                test_SSH = Connection.Ssh(result['Host'][i]["IP"], result['Host'][i]["Port"], result['Host'][i]["User"],result['Host'][i]["Password"], result['Command'][j])
-                info = test_SSH.method_processing_data()
-                host = result['Host'][i]["IP"]
+                info = test_SSH.stay_connection(result['Command'][j])
                 if result['Command'][j] == "nmcli connection show":
                     info_old = Connection.filter_data(keyword, info)
                     print(f'{host} 的筛选结果为：{info_old}')
                 else:
                     print(f'{host} 的ifconfig信息为：{info}')
+            test_SSH.close_connection()
 
 if __name__ == '__main__' :
     filter_Operation("test.yaml","ens192")
