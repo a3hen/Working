dbb75598252b6a3fbadf8adee61c0862bebda96f
a3hen <8675391+a3hen@user.noreply.gitee.com>
2022-05-27 12:24:27 +0800

    第二次练习

--- implement_reads.py
+++ implement_reads.py
@@ -1,31 +1,12 @@
 # -*- coding: utf-8 -*-
-<<<<<<< HEAD
 import re
 import paramiko
-=======
 
->>>>>>> dev
 import yaml
 import Connection
 
 
 def filter_Operation(yaml_name,keyword) :
-<<<<<<< HEAD
-    if __name__ == '__main__' :
-        with open(yaml_name, 'r', encoding='utf-8') as f:
-            result = yaml.load(f, yaml.FullLoader)
-        for i in range(len(result['Host'])):
-            for j in range(len(result['Command'])):
-                test_SSH = Connect.Ssh(result['Host'][i]["IP"], result['Host'][i]["Port"], result['Host'][i]["User"],result['Host'][i]["Password"], result['Command'][j])
-                info = test_SSH.method_processing_data()
-                host = result['Host'][i]["IP"]
-                if result['Command'][j] == "nmcli connection show":
-                    info_old = Connect.filter_data(keyword, info)
-                    print(f'{host} 的筛选结果为：{info_old}')
-                else:
-                    print(result['Host'][i]["IP"]+"的ifconfig信息为：\n"+str(info))
-                    print(f'{host} 的ifconfig信息为：{info}')
-=======
         with open(yaml_name, 'r', encoding='utf-8') as f:
             result = yaml.load(f, yaml.FullLoader)
 
@@ -41,7 +22,6 @@ def filter_Operation(yaml_name,keyword) :
                 else:
                     print(f'{host} 的ifconfig信息为：{info}')
             test_SSH.close_connection()
->>>>>>> dev
 
 if __name__ == '__main__' :
     filter_Operation("test.yaml","ens192")
