b8d6ada2485ca85ac9fb0a26e0af818879854a5b
a3hen <8675391+a3hen@user.noreply.gitee.com>
2022-05-27 15:54:29 +0800

    最终版

--- implement_reads.py
+++ implement_reads.py
@@ -16,19 +16,17 @@ def filter_operation(keyword) :
 
     with open(yaml_name, 'r', encoding='utf-8') as f:
         result = yaml.load(f, yaml.FullLoader)
-    for i in range(len(result['Host'])):
-    # for i in result['Host'] :
-        host = result['Host'][i]["IP"]
-        test_SSH = connection.Ssh(result['Host'][i]["IP"], result['Host'][i]["Port"], result['Host'][i]["User"],result['Host'][i]["Password"])
+    for i in result['Host'] :
+        host = i["IP"]
+        test_SSH = connection.Ssh(i["IP"],i["Port"],i["User"],i["Password"])
         test_SSH.start_connection()
-        for j in range(len(result['Command'])):
-        # for j in result['Command']:
-            info = test_SSH.stay_connection(result['Command'][j])
-            if result['Command'][j] == "nmcli connection show":
+        for j in result['Command'] :
+            info = test_SSH.stay_connection(j)
+            if j == 'nmcli connection show':
                 info_old = connection.filter_data(keyword, info)
-                print(f'{host} 的筛选结果为：{info_old}')
+                print(f'{host} 的ifconfig信息为：{info_old}')
             else:
-                print(f'{host} 的ifconfig信息为：{info}')
+                print(f'{host} 的free -m的信息为: {info}')
         test_SSH.close_connection()
 
 if __name__ == '__main__' :
