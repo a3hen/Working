9b12f3df06b0d63cb0b13a8c35704d14a8907b5e
a3hen <8675391+a3hen@user.noreply.gitee.com>
2022-05-27 14:28:09 +0800

    第五次提交

--- implement_reads.py
+++ implement_reads.py
@@ -1,25 +1,33 @@
 # -*- coding: utf-8 -*-
-
+import argparse
 import yaml
-import Connection
+import connection
 
 
-def filter_Operation(yaml_name,keyword) :
-        with open(yaml_name, 'r', encoding='utf-8') as f:
-            result = yaml.load(f, yaml.FullLoader)
+def filter_Operation(keyword) :
 
-        for i in range(len(result['Host'])):
-            host = result['Host'][i]["IP"]
-            test_SSH = Connection.Ssh(result['Host'][i]["IP"], result['Host'][i]["Port"], result['Host'][i]["User"],result['Host'][i]["Password"])
-            test_SSH.start_connection()
-            for j in range(len(result['Command'])):
-                info = test_SSH.stay_connection(result['Command'][j])
-                if result['Command'][j] == "nmcli connection show":
-                    info_old = Connection.filter_data(keyword, info)
-                    print(f'{host} 的筛选结果为：{info_old}')
-                else:
-                    print(f'{host} 的ifconfig信息为：{info}')
-            test_SSH.close_connection()
+    parser = argparse.ArgumentParser(description="输入yaml文件名")
+    parser.add_argument('-apply', '--yaml_name', type=str, help='yaml文件名')
+    args = parser.parse_args()
+    yaml_name = args.yaml_name
+
+
+    with open(yaml_name, 'r', encoding='utf-8') as f:
+        result = yaml.load(f, yaml.FullLoader)
+    for i in range(len(result['Host'])):
+    # for i in result['Host'] :
+        host = result['Host'][i]["IP"]
+        test_SSH = connection.Ssh(result['Host'][i]["IP"], result['Host'][i]["Port"], result['Host'][i]["User"],result['Host'][i]["Password"])
+        test_SSH.start_connection()
+        for j in range(len(result['Command'])):
+        # for j in result['Command']:
+            info = test_SSH.stay_connection(result['Command'][j])
+            if result['Command'][j] == "nmcli connection show":
+                info_old = connection.filter_data(keyword, info)
+                print(f'{host} 的筛选结果为：{info_old}')
+            else:
+                print(f'{host} 的ifconfig信息为：{info}')
+        test_SSH.close_connection()
 
 if __name__ == '__main__' :
-    filter_Operation("test.yaml","ens192")
+    filter_Operation("ens192")
